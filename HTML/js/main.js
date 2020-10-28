085ab9332d7025c55008c8e47297a9b3761952b8
Tony <56505654+666hesoyam@users.noreply.github.com>
2020-10-28 23:31:52 +0200

    upd

--- HTML/js/main.js
+++ HTML/js/main.js
@@ -3,8 +3,12 @@ $(function(){
     $("#included__main-navigation").load("template/main-navigation.html");
     $("#included__footer").load("template/footer.html");
     $("#included__sub-footer").load("template/sub-footer.html");
+    // any blocks
     $("#included__feedback-block").load("template/feedback-block.html");
     $("#included__kpi-block").load("template/kpi-block.html");
-    $("#included__aside-block").load("template/aside-block.html");
+    // aside block
+    $("#included__aside-search").load("template/aside-search.html");
+    $("#included__aside-news").load("template/aside-news.html");
+    $("#included__aside-action").load("template/aside-action.html");
 });
 
