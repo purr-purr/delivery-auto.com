f81d6cce91fce21b1ce015486719ef06ef0122cb
Tony <56505654+666hesoyam@users.noreply.github.com>
2020-11-10 17:19:32 +0200

    upd

---
+++ HTML/js/components/popup.js
@@ -0,0 +1,14 @@
+
+// pop up мероприятия на странице "Центр обслуговування клієнтів"
+$('#popup-event--1').popup({
+    pagecontainer: 'body',
+    closebutton: true,
+    escape: false
+});
+
+
+$('#popup-event--2').popup({
+    pagecontainer: 'body',
+    closebutton: true,
+    escape: false
+});
