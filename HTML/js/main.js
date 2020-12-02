3b4f85c8947b93873f2ad191110d54cb5faeece7
Tony <56505654+666hesoyam@users.noreply.github.com>
2020-12-02 23:27:48 +0200

    updd

--- HTML/js/main.js
+++ HTML/js/main.js
@@ -25,6 +25,7 @@ $(function(){
 
 // ---------------------------------------------------------------------------
 // Только для cl-program-layalty.html
+
 // Добавление и удаление расширеного списка баллов
 $('.more-point__btn').on('click', function(event){
     $(this).toggleClass('more-point__btn--hover');
@@ -32,8 +33,13 @@ $('.more-point__btn').on('click', function(event){
 });
 // ---------------------------------------------------------------------------
 
+
+
+
+
 // ---------------------------------------------------------------------------
 // Только для cl-express-delivery.html
+
 // Добавление и удаление Дополнительной посылки
 $('.del-order__parcel-btn').on('click', function(event){
     $('.add-parcel').toggleClass('add-parcel--show-block');
@@ -43,18 +49,56 @@ $('.add-parcel__delete').on('click', function(event){
     $('.add-parcel').removeClass('add-parcel--show-block');
 });
 
-
-
-
+// Появление инпута 3-тя особа
 $('#person-3dr').on('change', function() {
     $('.person-3dr__input').toggleClass('person-3dr__hidden', this.value != 'string:100000002');
 });
 
+// Появление инпута "Оберіть платника страховки"
 $('#declared-cost').on('change', function() {
     $('.insurance-block').toggleClass('insurance-block__hidden', this.value);
 });
 
+// Появление инпута "Оберіть платника страховки" для 3-тя особа
 $('#declared-cost__3dr').on('change', function() {
     $('.insurance-payer-3dr__input').toggleClass('insurance-payer-3dr__hidden', this.value != 'string:100000002');
+});
+// ---------------------------------------------------------------------------
+
+
+
+
+
+
+
+// ---------------------------------------------------------------------------
+// Только для cl-сourier-form.html
+
+// Добавление и удаление блока при выборе Тип вантажу: "Палети"
+$('#type-cargo').bind("change keyup",function() {
+    $(this).find(":selected").each(function () {
+        $('.only-for-pallets').toggleClass('show-block2');
+        $('.only-for-cargo').toggleClass('hide-block2');
+    });
+});
+
+// Открытие блока добавление Склада
+$('.add-warehouse__btn').on('click', function(event){
+    $('.add-parcel').toggleClass('add-parcel--show-block');
+});
+
+// Кнопка удаления блока Склада
+$('.add-parcel__delete').on('click', function(event){
+    $('.add-parcel').removeClass('add-parcel--show-block');
+});
+
+// Чекбокс Доупакувати вантаж, появление textarea при выборе
+$('.add-pack').on('click', function(event){
+    $('.add-pack__text-area').toggleClass('show-block');
+});
+
+// Забирання вантажу на кожен день, при выборе - появление всех дней нидели
+$('.day-vote').on('click', function(event){
+    $('.everyday_block').toggleClass('show-it');
 });
 // ---------------------------------------------------------------------------
