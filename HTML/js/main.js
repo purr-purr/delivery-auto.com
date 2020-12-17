ec5f91f28b140d445aff232648bdaa50363b3209
Tony <56505654+666hesoyam@users.noreply.github.com>
2020-12-17 22:08:32 +0200

    upd

--- HTML/js/main.js
+++ HTML/js/main.js
@@ -148,3 +148,51 @@ $('#time18').hover(
     function(){ $('.schedule').removeClass('schedule__time18') }
 );
 // ---------------------------------------------------------------------------
+
+
+
+// ---------------------------------------------------------------------------
+// Автоматическое открытие <Details> при переходе по якорю
+
+function MakeArrayOfAllPrefixes(str){
+    //console.log("MakeArrayOfAllPrefixes("+str+")");
+    var prefixes = [];
+    for (var i=1; i<=str.length; i++){
+  prefixes.push(str.substr(0,i));
+    }
+    console.log("MakeArrayOfAllPrefixes("+str+") -> returns [" + prefixes + "]");
+    return prefixes;
+  }
+  
+  function SetOpenAttrForIdsAndPrefixes(ids, openAttrBool){
+    $('details').attr('open',false); // close all others first
+    console.log("SetOpenAttrForIds([" +ids+"], "+openAttrBool+")");
+    for (idindex in ids) {
+  var id = ids[idindex];
+  if (id != ""){
+    var prefixes = MakeArrayOfAllPrefixes(id);
+    for (prefixidx in prefixes) {
+      var prefix = prefixes[prefixidx];
+      if(openAttrBool==true) { operationstr="Opening"; } else { operationstr="Closing"};
+      console.log(operationstr+" <details id='#"+prefix+"'> with $('#"+prefix+").attr('open',"+openAttrBool+");");
+      $('#'+prefix).attr('open',openAttrBool);
+    }
+  }
+    }
+  }
+  
+  function SetOpenAttrForIdsAndPrefixesFromLocationHash(){
+    var hashes = $(location).attr('hash').split('#'); 
+    SetOpenAttrForIdsAndPrefixes(hashes, true); 
+  }
+  
+  $(document).ready(function(){
+    SetOpenAttrForIdsAndPrefixesFromLocationHash();
+  
+    if ("onhashchange" in window) {// does the browser support the hashchange event?
+    window.onhashchange = function () {
+       SetOpenAttrForIdsAndPrefixesFromLocationHash();
+    }
+    }
+});
+// ---------------------------------------------------------------------------
